c31ae492db75aad8c2e2bbc515375ddf9f83ee24
=nonguyen1 <nonguyen@ucsd.edu>
2019-05-28 02:25:30 -0700

    Remove setting files

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PROJECT_DIR = os.path.dirname(__file__)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -117,8 +116,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'staticfiles'),
-)
 
 django_heroku.settings(locals())
